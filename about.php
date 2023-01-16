2edc4d7369e0368506c2d6b1f02b38a0a0981565
Hannane <hannanekabuli@gmail.com>
2023-01-16 13:46:40 +0100

    first commit

---
+++ about.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!-- swiper css link-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+
+    <!--- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!-- header section starts -->
+<section class="header">
+
+    <a href="home.php" class="logo">travel.</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+
+</section>
+
+
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/heading-bg-1.png) no-repeat">
+    <h1>about us</h1>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts-->
+
+<section class="footer">
+
+<div class="box-container">
+
+    <div class="box">
+        <h3>quick Links</h3>
+        <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+        <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+        <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+        <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+    </div>
+
+    <div class="box">
+        <h3>extra Links</h3>
+        <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+        <a href="#"> <i class="fas fa-angle-right"></i> term of use</a>
+    </div>
+
+    <div class="box">
+        <h3>contact info</h3>
+        <a href="#"> <i class="fas fa-phone"></i> +123 456 789</a>
+        <a href="#"> <i class="fas fa-phone"></i> +111 222 333</a>
+        <a href="#"> <i class="fas fa-envelope"></i> hannakabuli@yahoo.com</a>
+        <a href="#"> <i class="fas fa-map"></i> Gothenburg, sweden </a>
+    </div>
+
+    <div class="box">
+        <h3>follow us</h3>
+        <a href="#"> <i class="fab fa-facebook-f"></i> facebook</a>
+        <a href="#"> <i class="fab fa-twitter"></i> twitter</a>
+        <a href="#"> <i class="fab fa-instagram"></i> instagram</a>
+        <a href="#"> <i class="fab fa-linkedin"></i> linkedin</a>
+    </div>
+
+    <div class="credit"> creted by <span>mr. web designer</span> | all rights reserved! </div>
+
+
+</div>
+
+</section>
+
+<!-- swiper js link --->
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+    
+</body>
+</html>
