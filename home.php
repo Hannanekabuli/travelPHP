1aba90a87bf229452889d2198d3e32f5879cbddf
Hannane <hannanekabuli@gmail.com>
2023-01-18 13:19:50 +0100

    slide

--- home.php
+++ home.php
@@ -37,6 +37,41 @@
 
 <!-- home section starts -->
 
+<section class="home">
+    <div class="swiper home-slider">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide" style="background:url(images/home-slide-1.png)">
+                <div class="content">
+                    <span>explore, discover, travel</span>
+                    <h3> travel arround the world</h3>
+                    <a href="package.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide" style="background:url(images/home-slide-2.png)">
+                <div class="content">
+                    <span>explore, discover, travel</span>
+                    <h3> discover the new places</h3>
+                    <a href="package.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide" style="background:url(images/home-slide-3.png)">
+                <div class="content">
+                    <span>explore, discover, travel</span>
+                    <h3> make </h3>
+                    <a href="package.php" class="btn">discover more</a>
+                </div>
+            </div>
+
+        </div>
+
+        </div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+    </div>
+    </div>
+</section>
 
 
 <!-- services section starts -->
@@ -81,7 +116,6 @@
 </section>
 
 <!-- services section ends -->
-
 
 
 
